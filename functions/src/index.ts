e7488ab59c6cfe3f7c4e910dbd3bee04c124d50b
42sora <hurysy@gmail.com>
2019-03-20 15:12:51 +0900

    add pubsub

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,57 @@
 import * as functions from 'firebase-functions'
 import * as puppeteer from 'puppeteer'
+const { PubSub } = require('@google-cloud/pubsub')
+const pubsub = new PubSub('akuu-37e4b')
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions
+const launchOptions = {
+  args: ['--no-sandbox']
+}
+
+const TOPIC_SCRAPING_FORTUNE = 'ScrapingFortune'
+
+export const startScraping = functions
+  .region('asia-northeast1')
+  .https.onRequest((request, response) => {
+    if (request.method !== 'POST') {
+      response.status(405).send()
+      return
+    }
+    const userID = request.body.userID
+    const email = request.body.email
+    const password = request.body.password
+    if (!userID || !email || !password) {
+      response.status(400).send()
+      return
+    }
+
+    const message = { userID, email, password }
+    console.log(message)
+
+    const dataBuffer = Buffer.from(JSON.stringify(message))
+    pubsub
+      .topic(TOPIC_SCRAPING_FORTUNE)
+      .publish(dataBuffer)
+      .then((messageId: any) => {
+        console.log(`Message ${messageId} published.`)
+      })
+      .catch((err: any) => {
+        console.error('ERROR:', err)
+      })
+    response.send('Hello from Firebase!')
+  })
+
+export const pubFortune = functions
+  .region('asia-northeast1')
   .runWith({
     timeoutSeconds: 300,
     memory: '1GB'
   })
-  .https.onRequest(async (request, response) => {
-    const launchOptions = {
-      args: ['--no-sandbox']
-    }
-
+  .pubsub.topic(TOPIC_SCRAPING_FORTUNE)
+  .onPublish(async (message, context) => {
+    console.log('message' + message.data)
+    console.log('message' + message.json)
+    console.log('message' + message.toJSON())
     const browser = await puppeteer.launch(launchOptions)
     const page = await browser.newPage()
-
-    response.send('Hello from Firebase!' + page.url())
+    console.log(page.url())
   })
