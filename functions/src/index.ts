1c8f35ef4944f89ca71a9294283b59b6c60c6a5a
42sora <hurysy@gmail.com>
2019-03-20 10:48:42 +0900

    add function HelloWorld

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,21 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
+import * as puppeteer from 'puppeteer'
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+export const helloWorld = functions
+  .runWith({
+    timeoutSeconds: 300,
+    memory: '1GB'
+  })
+  .https.onRequest(async (request, response) => {
+    const launchOptions = {
+      args: ['--no-sandbox']
+    }
+
+    const browser = await puppeteer.launch(launchOptions)
+    const page = await browser.newPage()
+
+    response.send('Hello from Firebase!' + page.url())
+  })
