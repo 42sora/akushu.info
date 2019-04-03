f9af6153be4604208869d8cf45aee6d5980e75f5
42sora <hurysy@gmail.com>
2019-04-03 14:10:58 +0900

    fix fontawesome to lazy import

--- src/main.js
+++ src/main.js
@@ -4,9 +4,10 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import './firebaseConfig'
-import '@fortawesome/fontawesome-free/js/all'
 
 Vue.config.productionTip = false
+
+import(/* webpackChunkName: "fontawesome" */'@fortawesome/fontawesome-free/js/all')
 
 new Vue({
   router,
