6868b64596fc60cc26573290d74ad5cfa5a61f64
42sora <hurysy@gmail.com>
2019-07-09 14:54:20 +0900

    Fix style utils

--- src/utils/StyleUtil.js
+++ src/utils/StyleUtil.js
@@ -1,19 +1,27 @@
-export const getBkColerClass = (value) => {
-  return {
-    'bk-coler-nogi': value.includes('乃木坂'),
-    'bk-coler-keyaki': value.includes('欅坂'),
-    'bk-coler-yoshimoto': value.includes('吉本坂'),
-    'bk-coler-hinata': value.includes('日向坂'),
-    'bk-coler-zenkoku': value.includes('全国'),
-    'bk-coler-kobetsu': value.includes('個別'),
-    'bk-coler-miyagi': value.includes('宮城'),
-    'bk-coler-chiba': value.includes('千葉'),
-    'bk-coler-tokyo': value.includes('東京'),
-    'bk-coler-kanagawa': value.includes('神奈川'),
-    'bk-coler-aichi': value.includes('愛知'),
-    'bk-coler-kyoto': value.includes('京都'),
-    'bk-coler-osaka': value.includes('大阪'),
-    'bk-coler-kanto': value.includes('関東'),
-    'bk-coler-kansai': value.includes('関西')
+const config = [
+  { value: '乃木坂', class: ['bk-coler-nogi'] },
+  { value: '欅坂', class: ['bk-coler-keyaki'] },
+  { value: '吉本坂', class: ['bk-coler-yoshimoto'] },
+  { value: '日向坂', class: ['bk-coler-hinata'] },
+  { value: '全国', class: ['bk-coler-zenkoku'] },
+  { value: '個別', class: ['bk-coler-kobetsu'] },
+  { value: '宮城', class: ['bk-coler-miyagi'] },
+  { value: '千葉', class: ['bk-coler-chiba'] },
+  { value: '東京', class: ['bk-coler-tokyo'] },
+  { value: '神奈川', class: ['bk-coler-kanagawa'] },
+  { value: '愛知', class: ['bk-coler-aichi'] },
+  { value: '京都', class: ['bk-coler-kyoto'] },
+  { value: '大阪', class: ['bk-coler-osaka'] },
+  { value: '関東', class: ['bk-coler-kanto'] },
+  { value: '関西', class: ['bk-coler-kansai'] }
+]
+
+export const getBkColerClass = value => {
+  for (let i = 0; i < config.length; i++) {
+    const element = config[i]
+    if (value.includes(element.value)) {
+      return element.class
+    }
   }
+  return []
 }
